312b38e24401c314248342413eb0c509ade99b4d
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-23 20:24:03 +0100

    5.5

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/api/Strategy.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/api/Strategy.java
@@ -1,8 +1,6 @@
 package it.unibo.generics.graph.api;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public interface Strategy<T> {
     public List<T> apply(T source, T target, List<T> tmpResult, List<T> visited, Graph<T> graph);
